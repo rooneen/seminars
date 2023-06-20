d4333fe8d75b919fbb668c3feaf4591ed8ad5033
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-20 20:58:26 +0300

    add sem3 exemple3

---
+++ BazovieAlgoritmi/seminar3_17.06.23/exemple3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до n
+
+Console.Write("Введите число: ");
+
+int N = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= N; i++)
+{
+    int sqr = Convert.ToInt32(Math.Pow(i, 2));
+    Console.WriteLine(sqr);
+}
