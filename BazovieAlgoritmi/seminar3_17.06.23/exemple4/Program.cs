e3b3694fa02dd1ce4009207ba506f236d2ac0508
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-20 21:36:43 +0300

    add sem3 exemple4

---
+++ BazovieAlgoritmi/seminar3_17.06.23/exemple4/Program.cs
@@ -0,0 +1,16 @@
+﻿// проверка равенства первого и третьего числа
+
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine()!;
+
+if(number.Length == 3){
+    if(number[0] == number[2]){
+        Console.WriteLine("Числа равны");
+    }
+    else{
+        Console.WriteLine("Числа не равны");
+    }
+}
+else{
+    Console.WriteLine("Введено не 3х значное число");
+}
