5dbd33bfb0abb2fe72fd913830ecb34ce8e4e7d1
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-20 20:31:04 +0300

    add sem3 exemple2

--- BazovieAlgoritmi/seminar3_17.06.23/exemple2/Program.cs
+++ BazovieAlgoritmi/seminar3_17.06.23/exemple2/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и нахолит расстояние между ними в 2D пространстве
 
+
+Console.Write("Введите координату Х точки А: ");
+int ax = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату У точки А: ");
+int ay = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Х точки В: ");
+int bx = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату У точки В: ");
+int by = int.Parse(Console.ReadLine()!);
+
+
+double AB = Math.Sqrt(Math.Pow((bx - ax), 2) +Math.Pow((by-ay), 2)); //Math.Sqrt - вычесть корень, Math.Pow - возвести в степень (степень после запятой)
+
+Console.WriteLine($"Расстояние между точками = {AB:f2}"); // :fn - округляет до "n" символов после запятой
+
+
