839e8e30a7dd65bf6c3dac26becdd9d1d66337cb
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-21 21:30:30 +0300

    add sem4  exemple2.2

--- BazovieAlgoritmi/seminar4_21.06.23/exemple2/Program.cs
+++ BazovieAlgoritmi/seminar4_21.06.23/exemple2/Program.cs
@@ -1,30 +1,31 @@
 ﻿// Напишите программу которая принимает на вход число и выдает количество цифр в числе
 
-// Console.Write("Введите число: ");
-// string number = Console.ReadLine()!;
-// Console.WriteLine($"В числе {number} - {SumNumb(number)} символов");
-
-// int SumNumb(int [] array)
-// {
-//     int count = number.Length;
-
-//     for (int i = 0; i < count; i++)
-//     {
-//         Console.Write($"{count} ");
-//     }
-//     return count;
-// }    
-
 Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
+int number = Convert.ToInt32(Console.ReadLine());
+string numberString = Convert.ToString(number);
 Console.WriteLine($"В числе {number} - {SumNumb(number)} символов");
 
-int SumNumb(int A){
+int SumNumb(int array)
+{
+    int count = numberString.Length;
 
-    int count = 0;
-    while(A > 0){
-        count ++;
-        A /= 10; // A = A / 10
+    for (int i = 0; i < count; i++)
+    {
+        //Console.Write($"{count} ");
     }
     return count;
-}
+}    
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"В числе {number} - {SumNumb(number)} символов");
+
+// int SumNumb(int A){
+
+//     int count = 0;
+//     while(A > 0){
+//         count ++;
+//         A /= 10; // A = A / 10
+//     }
+//     return count;
+// }
