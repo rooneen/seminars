fd27108966cc480d3e3fcfc945517eb7ec3f1fd3
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-21 15:21:18 +0300

    add exemple5

---
+++ BazovieAlgoritmi/seminar3_17.06.23/exemple5/Program.cs
@@ -0,0 +1,36 @@
+﻿// отсортировать числа в массиве по возростанию
+
+int[] arr = {1, 5, 3, 7, 4, 6, 2, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine(); // распечатать масив в терминал
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length;j ++)
+        {
+            if(array[j] < array[minPosition])
+            {
+                minPosition = j; // находит мах элемент
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
