16444d1242a1ed6b3455f634479b576dbcbdaffe
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-21 16:17:01 +0300

    add sem4  exemple1

---
+++ BazovieAlgoritmi/seminar4_21.06.23/exemple1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+Console.Write("Введите число: ");
+int limit = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {limit} = {GetSum(limit)}");
+
+//функция подсчета суммы от 1 до А
+int GetSum(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)    
+    {
+     sum += i; //sum = sum + i;    
+    }
+    return sum;
+}
