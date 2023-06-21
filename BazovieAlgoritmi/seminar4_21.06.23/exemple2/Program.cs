0da8c2bf685d25f7b3703e1cda8113c9d10ee966
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-21 20:38:03 +0300

    add sem4  exemple2

---
+++ BazovieAlgoritmi/seminar4_21.06.23/exemple2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу которая принимает на вход число и выдает количество цифр в числе
+
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine()!;
+// Console.WriteLine($"В числе {number} - {SumNumb(array)} символов");
+
+// int SumNumb(int [] array)
+// {
+//     int count = number.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{count} ");
+//     }
+//     return count;
+// }    
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"В числе {number} - {SumNumb(number)} символов");
+
+int SumNumb(int A){
+
+    int count = 0;
+    while(A > 0){
+        count ++;
+        A /= 10; // A = A / 10
+    }
+    return count;
+}
