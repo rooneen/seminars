3283df6fc7436156c0be2e84d2aad7cd4e194d56
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-22 22:32:38 +0300

    add sem4  exemple4

--- BazovieAlgoritmi/seminar4_21.06.23/exemple4/Program.cs
+++ BazovieAlgoritmi/seminar4_21.06.23/exemple4/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 
+int[] GetArray(int size)
+{
+    int[] result = new int[size]; // создает целочисленный массив с размером size
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);        
+    }
+    return result;
+}
+
+int[] array = GetArray(8);
+Console.WriteLine($"[{String.Join(",", array)}]"); //String.Join вывод строки через разделитель (работает только с строками и одномерными массивами)
