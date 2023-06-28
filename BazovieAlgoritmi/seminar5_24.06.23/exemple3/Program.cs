b9cb4e859710ad5fc781943c1fc87a7ca2b4e076
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-28 22:39:10 +0300

    seminar 5 exemple2(положительные элементы замените на соответствующие отрицательные и наоборот)

---
+++ BazovieAlgoritmi/seminar5_24.06.23/exemple3/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные и наоборот
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+
+int[] array = GetArray(size, -999, 999);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+for(int i = 0; i < size; i++)
+{
+    array[i] *= -1;
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{         
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
