2ad3bf87effc6d403f9020e88256af9deb5aa036
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-28 23:43:14 +0300

    seminar 5 exemple4(Найдите количество элементов массива значения которых лежат в отрезке [10, 99])

---
+++ BazovieAlgoritmi/seminar5_24.06.23/exemple4/Program.cs
@@ -0,0 +1,24 @@
+﻿// задайте одномерный массив из 123 случайных чисел 
+// Найдите количество элементов массива значения которых лежат в отрезке [10, 99]
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] array = GetArray(size, 1, 999);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int count = 0;
+
+foreach (int el in array)
+{
+    count += el <=99 && el >= 10 ? 1 : 0;
+}
+Console.WriteLine($"количество элементов массива от 10 до 99 = {count}");
+
+int[] GetArray(int size, int minValue, int maxValue){         
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
