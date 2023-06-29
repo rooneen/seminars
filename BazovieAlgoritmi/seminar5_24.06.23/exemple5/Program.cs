0a456a0c1c33f21bfbfe8d57fc7b3bbb330d0e02
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-29 22:04:51 +0300

    seminer6 exemple1 первый вариант разворота массива

---
+++ BazovieAlgoritmi/seminar5_24.06.23/exemple5/Program.cs
@@ -0,0 +1,19 @@
+﻿// найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
+// второй и предпоследний и тд
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] numbers = new int[size];
+numbers = GetArray(numbers, 1, 10);
+
+
+
+int[] GetArray(int size, int minValue, int maxValue){         
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
---
+++ BazovieAlgoritmi/seminar6_29.06.23/exemple1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, котрая перевернет одномерный массив (последний элемент будет на первом месте, а первый на последнем и т.д.)
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] array = GetArray(size, 10, 99);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+ReverseArray1(array);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+//первый вариант разворота массива
+void ReverseArray1(int[] inArray)
+{
+    for(int i = 0; i < inArray.Length / 2; i++)
+    {
+        int temp = inArray[i]; // временная переменная для первого элемента массива
+        inArray[i] = inArray[inArray.Length - 1 - i]; // переносим в первый элемент - последний элемент
+        inArray[inArray.Length - 1 - i] = temp; // переносим в последний элемент значение первого элемента и временной переменной
+    }
+}
+
+int[] GetArray(int size, int minValue, int maxValue){         
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
