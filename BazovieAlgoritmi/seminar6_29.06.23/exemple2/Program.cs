247ea4944af9497b8118a071e7f51d5b9e30af1a
Davidenko Aleksey <davaleks82@mail.ru>
2023-06-29 23:01:09 +0300

    seminer6 exemple2 может ли существовать треугольник с сторонами такой длинны

---
+++ BazovieAlgoritmi/seminar6_29.06.23/exemple2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с сторонами такой длинны
+// теорема - каждая сторона треугольника меньше суммы двух других сторон
+
+Console.Write("Введиде длинну стороны А: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введиде длинну стороны B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введиде длинну стороны C: ");
+int C = int.Parse(Console.ReadLine()!);
+
+if(GatTriangle(A, B, C)) Console.WriteLine($"Треугольник со сторонами {A} {B} {C} существует");
+else Console.Write($"Треугольник со сторонами {A} {B} {C} не существует");
+
+bool GatTriangle(int A, int B, int C)
+{
+    if((A < B + C) && (B < A + C) && (C < A + B)) return true;
+    else return false;
+}
