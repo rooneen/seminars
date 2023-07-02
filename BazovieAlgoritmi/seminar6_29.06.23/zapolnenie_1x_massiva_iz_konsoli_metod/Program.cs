e144a405773362daa1c1e2fba9cbf06be865d01d
Davidenko Aleksey <davaleks82@mail.ru>
2023-07-02 12:57:08 +0300

    exemple 4 Заполнение 1х массива вручную из консоли (метод)

---
+++ BazovieAlgoritmi/seminar6_29.06.23/zapolnenie_1x_massiva_iz_konsoli_metod/Program.cs
@@ -0,0 +1,15 @@
+﻿// Метод заполнения одномерного массива в ручную из консоли
+
+Console.Write("Введите числа через пробел: ");
+int[] numbers = GetArrayFromString(Console.ReadLine()!);
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+}
