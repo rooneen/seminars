17f3fd5dffc2f2dea76307bb0192283c11dd0dca
Davidenko Aleksey <davaleks82@mail.ru>
2023-07-02 13:34:18 +0300

    seminar7 exemple1 Задайте двумерный массив размером m×n, заполненный случайными целыми числами

--- BazovieAlgoritmi/seminar7_02.07.23/exemple1/Program.cs
+++ BazovieAlgoritmi/seminar7_02.07.23/exemple1/Program.cs
@@ -1,3 +1,42 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
 
 
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int colums = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, colums, 0, 10);
+PrintArray(array);
+
+//Метод заполнения двумерного массива
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue,maxValue - 1);
+        }
+    }
+    return result;
+}
+
+
+//Метод вывода двумерного массива
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
