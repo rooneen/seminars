696733abcfc97ca6eb93cd547672d194139477cc
Davidenko Aleksey <davaleks82@mail.ru>
2023-07-02 13:14:44 +0300

    Метод заполнения двумерного массива new Random

---
+++ BazovieAlgoritmi/seminar7_02.07.23/metod_zapolnenia_2x_massiva/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Метод заполнения двумерного массива
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue,maxValue - 1);
+        }
+    }
+    return result;
+}
