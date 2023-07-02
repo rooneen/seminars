e6f1c3276043b4baffeac77a3bba0ee7c3a1cd23
Davidenko Aleksey <davaleks82@mail.ru>
2023-07-02 13:26:01 +0300

    Метод вывода двумерного массива

--- BazovieAlgoritmi/seminar7_02.07.23/metod_zapolnenia_2x_massiva/Program.cs
+++ BazovieAlgoritmi/seminar7_02.07.23/metod_zapolnenia_2x_massiva/Program.cs
@@ -13,3 +13,19 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     }
     return result;
 }
+
+
+//Метод вывода двумерного массива
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
