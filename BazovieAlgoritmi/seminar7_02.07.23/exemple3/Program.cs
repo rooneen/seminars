59226f0ec42554036b6d4faca073a8ddaec05fbc
Davidenko Aleksey <davaleks82@mail.ru>
2023-07-06 23:11:35 +0300

    seminar7 exemple3 Задайте двумерный массив, Найдите элементы у которых оба индекса четные и замените эти элементы на их квадраты

--- BazovieAlgoritmi/seminar7_02.07.23/exemple3/Program.cs
+++ BazovieAlgoritmi/seminar7_02.07.23/exemple3/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿// Задайте двумерный массив, Найдите элементы у которых оба индекса четные и замените эти элементы на их квадраты
+
+
 
 Console.Write("Введите количество строк: ");
 int rows = int.Parse(Console.ReadLine()!);
@@ -9,20 +11,37 @@ int colums = int.Parse(Console.ReadLine()!);
 int[,] array = GetArray(rows, colums, 0, 10);
 PrintArray(array);
 
+Console.WriteLine();
+PrintArray(GetPow(array));
 
 //Метод заполнения двумерного массива
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-    int[,] result = new int[m,n];
+    int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i,j] = new Random().Next(minValue,maxValue - 1);
+            result[i, j] = new Random().Next(minValue, maxValue - 1);
         }
     }
     return result;
+}
+
+int[,] GetPow(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                Array[i, j] = Convert.ToInt32(Math.Pow(Array[i, j], 2));
+            }
+        }
+    }
+    return Array;
 }
 
 
@@ -34,9 +53,9 @@ void PrintArray(int[,] inArray)
     {
         for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            Console.Write($"{inArray[i,j]} ");
+            Console.Write($"{inArray[i, j]} ");
         }
         Console.WriteLine();
     }
-    
+
 }
