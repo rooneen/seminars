66f0cd4c2ff191232138c0d21bdbb505312f1ed2
Davidenko Aleksey <davaleks82@mail.ru>
2023-07-15 11:10:59 +0300

    Seminar9  exemple3 - выведет все натуральные числа в промежутке от M до N. Выполнить с помощью рекурсии.

---
+++ BazovieAlgoritmi/seminar9_15.07.23/exemple3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Выполнить с помощью рекурсии.
+
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(M, N));
+
+string PrintNumbers(int start, int end)
+{
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
